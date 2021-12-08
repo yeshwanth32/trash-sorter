af2248de952816c19887cd7d4f31e74a799cdef2
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-08 03:08:30 -0500

    removed unnecessary stuff

--- obj_test.py
+++ obj_test.py
@@ -7,6 +7,8 @@ import os, subprocess, re
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+# import matplotlib
+# matplotlib.use('GTK3Cairo')  # or 'GTK3Cairo'
 import shutil
 import json
 from google.protobuf import text_format
@@ -14,61 +16,21 @@ import pprint
 import random
 from pathlib import Path
 from google.protobuf import text_format
-thres = 0.45 # Threshold to detect object
-nms_threshold = 0.2
-# cap = cv2.VideoCapture(0)
-# # cap.set(3,1280)
-# # cap.set(4,720)
-# # cap.set(10,150)
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+import cv2
 
-# # cap = cv2.VideoCapture(0)
-# # cap.set(3,1280)
-# # cap.set(4,720)
-# # cap.set(10,70)
-
-# classNames= []
-# classFile = r'C:\Users\yeshw\OneDrive\Desktop\Desktop\Umass\UMass\Umass\CICS256\final_project\trash_sorter\trash-sorter\coco.names'
-# with open(classFile,'rt') as f:
-#     classNames = f.read().rstrip('\n').split('\n')
-# print(len(classNames))
-# #image =cv2.imread(r"C:\Users\yeshw\OneDrive\Desktop\Desktop\Umass\UMass\Umass\CICS256\final_project\trash_sorter\trash-sorter\lena.png", cv2.IMREAD_COLOR)
-# # cv2.imshow("OpenCV Image Reading", image)
-# # cv2.waitKey(0)
-# # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-# # plt.show()
-# configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-# weightsPath = 'frozen_inference_graph.pb'
-
-# net = cv2.dnn_DetectionModel(weightsPath,configPath)
-# net.setInputSize(320,320)
-# net.setInputScale(1.0/ 127.5)
-# net.setInputMean((127.5, 127.5, 127.5))
-# net.setInputSwapRB(True)
-
-# while True:
-#     success,img = cap.read()
-#     classIds, confs, bbox = net.detect(img,confThreshold=thres)
-#     bbox = list(bbox)
-#     confs = list(np.array(confs).reshape(1,-1)[0])
-#     confs = list(map(float,confs))
-#     #print(type(confs[0]))
-#     #print(confs)
-
-#     indices = cv2.dnn.NMSBoxes(bbox,confs,thres,nms_threshold)
-#     print(indices)
-
-#     for i in indices:
-#         #i = i[0]
-#         box = bbox[i]
-#         x,y,w,h = box[0],box[1],box[2],box[3]
-#         cv2.rectangle(img, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
-#         cv2.putText(img,classNames[classIds[i][0]-1].upper(),(box[0]+10,box[1]+30),
-#         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-#     cv2.imshow("Output",img)
-#     cv2.waitKey(1)
+import tkinter as tk
+from PIL import ImageTk, Image
 
 pb_path = 'ssd_mobilenet_v2_taco_2018_03_29.pb'
+LABEL_PATH = "labelmap.pb.txt"
+NCLASSES = 60
+
+label_map = label_map_util.load_labelmap(LABEL_PATH)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NCLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
 def reconstruct(pb_path):
     if not os.path.isfile(pb_path):
         print("Error: %s not found" % pb_path)
@@ -83,7 +45,6 @@ def reconstruct(pb_path):
             tf.import_graph_def(od_graph_def, name='')
     print("Success!")
     return detection_graph
-detection_graph = reconstruct(pb_path=pb_path)
 def image2np(image):
     (w, h) = image.size
     return np.array(image.getdata()).reshape((h, w, 3)).astype(np.uint8)
@@ -91,7 +52,8 @@ def image2np(image):
 def image2tensor(image):
     npim = image2np(image)
     return np.expand_dims(npim, axis=0)
-def detect(detection_graph, test_image_path):
+
+def detect_picture(detection_graph, test_image_path):
     with detection_graph.as_default():
         gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.01)
         with tf.compat.v1.Session(graph=detection_graph,config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)) as sess:
@@ -106,69 +68,29 @@ def detect(detection_graph, test_image_path):
                 [detection_boxes, detection_scores, detection_classes, num_detections],
                 feed_dict={image_tensor: image2tensor(image)}
             )
-
+            print(np.squeeze(scores))
             npim = image2np(image)
-            # vis_util.visualize_boxes_and_labels_on_image_array(
-            #     npim,
-            #     np.squeeze(boxes),
-            #     np.squeeze(classes).astype(np.int32),
-            #     np.squeeze(scores),
-            #     category_index,
-            #     use_normalized_coordinates=True,
-            #     line_thickness=15)
-            # plt.figure(figsize=(12, 8))
-            # plt.imshow(npim)
-            # plt.show()
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                npim,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=30,
+                min_score_thresh=.6)
+            temp = [category_index.get(i) for i in classes[0]]
+            print(temp)
+            for i in range(0, len(temp)):
+                if (temp[i] != None):
+                    print(temp[i])
+                    break
+            
+            plt.figure(figsize=(12, 8))
+            #cv2.imshow("Output",npim)
+            plt.imshow(npim, interpolation='nearest')
+            plt.savefig("taco_test.jpg")
 
-detect(detection_graph, 'C:\\Users\\yeshw\\OneDrive\\Desktop\\Desktop\\Umass\\UMass\\Umass\\CICS256\\final_project\\trash_sorter\\trash-sorter\\lena.png')
-import cv2
-import numpy as np
-thres = 0.45 # Threshold to detect object
-nms_threshold = 0.2
-cap = cv2.VideoCapture(0)
-# cap.set(3,1280)
-# cap.set(4,720)
-# cap.set(10,150)
+trained_detection_graph = reconstruct('frozen_inference_graph_taco.pb')
+detect_picture(trained_detection_graph, 'C:\\Users\\yeshw\\OneDrive\\Desktop\\Desktop\\Umass\\UMass\\Umass\\CICS256\\final_project\\trash_sorter\\trash-sorter\\cup.jpg')
 
-classNames= []
-classFile = r'C:\Users\yeshw\OneDrive\Desktop\Desktop\Umass\UMass\Umass\CICS256\final_project\trash_sorter\trash-sorter\coco.names'
-with open(classFile,'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
-print(len(classNames))
-
-#print(classNames)
-#configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
-# configPath = 'ssd_mobilenet_v2_taco_2018_03_29.pb'
-# weightsPath = 'frozen_inference_graph_taco.pb'
-
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/ 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-
-while True:
-    success,img = cap.read()
-    classIds, confs, bbox = net.detect(img,confThreshold=thres)
-    bbox = list(bbox)
-    confs = list(np.array(confs).reshape(1,-1)[0])
-    confs = list(map(float,confs))
-    #print(type(confs[0]))
-    #print(confs)
-
-    indices = cv2.dnn.NMSBoxes(bbox,confs,thres,nms_threshold)
-    #print(indices)
-    #break
-
-    for i in indices:
-        #i = i[0]
-        box = bbox[i]
-        x,y,w,h = box[0],box[1],box[2],box[3]
-        cv2.rectangle(img, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
-        cv2.putText(img,classNames[classIds[i]-1].upper(),(box[0]+10,box[1]+30),
-        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-    cv2.imshow("Output",img)
-    cv2.waitKey(1)
