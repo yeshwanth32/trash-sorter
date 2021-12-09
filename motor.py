4609468270d4c5c0be597f2623e8669db9586237
Brijesh Vyas <71729108+brijvyas@users.noreply.github.com>
2021-12-09 02:15:26 -0500

    motor 2:15am

--- motor.py
+++ motor.py
@@ -1,4 +1,43 @@
-from gpiozero import Motor
+# Import required libraries
+import sys
+import time
+import RPi.GPIO as GPIO
 
-motor = Motor(17, 26)
-motor.forward()
+# Use BCM GPIO references
+# instead of physical pin numbers
+#GPIO.setmode(GPIO.BCM)
+mode=GPIO.getmode()
+print " mode ="+str(mode)
+GPIO.cleanup()
+
+# Define GPIO signals to use - not me
+# Physical pins 11,15,16,18 - not me
+# GPIO17,GPIO22,GPIO23,GPIO24 - not me
+
+StepPinForward=6
+StepPinBackward=13
+sleeptime=1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(StepPinForward, GPIO.OUT)
+GPIO.setup(StepPinBackward, GPIO.OUT)
+
+def forward(x):
+    GPIO.output(StepPinForward, GPIO.HIGH)
+    print "forwarding running  motor "
+    time.sleep(x)
+    GPIO.output(StepPinForward, GPIO.LOW)
+
+def reverse(x):
+    GPIO.output(StepPinBackward, GPIO.HIGH)
+    print "backwarding running motor"
+    time.sleep(x)
+    GPIO.output(StepPinBackward, GPIO.LOW)
+
+print "forward motor "
+forward(5)
+print "reverse motor"
+reverse(5)
+
+print "Stopping motor"
+GPIO.cleanup()
