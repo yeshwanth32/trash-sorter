aa85432b2501e18a71fca7da61c14d81568b4223
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 02:51:05 -0500

    update dist sensor

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,6 +1,6 @@
 from time import sleep
 from gpiozero import DistanceSensor
-dist_sensor = DistanceSensor(echo=23, trigger=24, max_distance=4)
+dist_sensor = DistanceSensor(echo=13, trigger=6, max_distance=4)
 print("Press CTRL-C to exit.\n")
 while True:
     print("Distance sensor read %.1f cm." % (dist_sensor.distance * 100))
