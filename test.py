b57eabda039961910f618839a7d6082080602adf
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 07:44:51 -0500

    camera

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-from obj_test import *
+from picamera import PiCamera, Color
+from time import sleep
 
-detect_picture()
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Desktop/camera_image.jpg')
+camera.stop_preview()
