baea4afba4f1a37f2bc18b6bc9d2a364bf3c00a8
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 16:12:38 -0500

    updated sender

--- receiver.py
+++ receiver.py
@@ -4,10 +4,7 @@ from npsocket import SocketNumpyArray
 
 sock_receiver = SocketNumpyArray()
 sock_receiver.initalize_receiver(9999)
-
-while True:
-    frame = sock_receiver.receive_array()
-
-    # Display
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+frame = sock_receiver.receive_array()
+# Display
+cv2.imshow('frame', frame)
+cv2.waitKey(1)
--- sender.py
+++ sender.py
@@ -1,13 +1,17 @@
 
 from npsocket import SocketNumpyArray
-import cv2
+#import cv2
 
-cap = cv2.VideoCapture(0)
-sock_sender = SocketNumpyArray()
+# cap = cv2.VideoCapture(0)
+# sock_sender = SocketNumpyArray()
 
 sock_sender.initialize_sender('192.168.114.166', 9999)
+image = PIL.Image.open("cup.jpg")
+image_array = np.array(image)
+sock_sender.send_numpy_array(image_array)
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (620, 480))
-    sock_sender.send_numpy_array(frame)
+# while True:
+    
+#     ret, frame = cap.read()
+#     frame = cv2.resize(frame, (620, 480))
+    
