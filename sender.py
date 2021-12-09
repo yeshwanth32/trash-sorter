47ad774587678b341970a7cc0fb518d17b81e96d
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 16:47:28 -0500

    made changes to sender

--- sender.py
+++ sender.py
@@ -1,14 +1,21 @@
 
 from npsocket import SocketNumpyArray
+from PIL import Image
+import PIL
+import numpy as np
 #import cv2
 
 # cap = cv2.VideoCapture(0)
-# sock_sender = SocketNumpyArray()
+sock_sender = SocketNumpyArray()
 
-sock_sender.initialize_sender('192.168.114.166', 9999)
-image = PIL.Image.open("cup.jpg")
+sock_sender.initialize_sender('172.20.10.6', 7245)
+
+
+image = PIL.Image.open("camera_image.jpg")
 image_array = np.array(image)
 sock_sender.send_numpy_array(image_array)
+response = sock_sender.receive_array()
+print(response)
 
 # while True:
     
