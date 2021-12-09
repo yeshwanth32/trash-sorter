1c492e83bcb9cae366ddbb9f0b7891680e7c623a
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 17:53:52 -0500

    possibly final update

--- receiver.py
+++ receiver.py
@@ -5,11 +5,11 @@ from PIL import Image
 from obj_test import *
 
 
-while True:
-    sock_receiver = SocketNumpyArray()
-    sock_receiver.initalize_receiver(7245)
+sock_receiver = SocketNumpyArray()
+sock_receiver.initalize_receiver(7245)
 
-    frame = sock_receiver.receive_array()  # Receiving the image as numpy array. 
-    pil_image=Image.fromarray(frame)
-    pil_image.save("received_image.jpg")
-    detect_picture("received_image.jpg")
+frame = sock_receiver.receive_array()  # Receiving the image as numpy array. 
+pil_image=Image.fromarray(frame)
+pil_image.save("received_image.jpg")
+detect_picture("received_image.jpg")
+    
--- sender.py
+++ sender.py
@@ -3,17 +3,31 @@ from npsocket import SocketNumpyArray
 from PIL import Image
 import PIL
 import numpy as np
+from picamera import PiCamera
+from time import sleep
+
 #import cv2
 
 # cap = cv2.VideoCapture(0)
 sock_sender = SocketNumpyArray()
+camera = PiCamera()
 
-sock_sender.initialize_sender('172.20.10.6', 7245)
+sock_sender.initialize_sender('192.168.201.166', 7245)
+print("motion detected...camera warming up")
+camera.start_preview()
+sleep(3)
+camera.capture('camera_image.jpg')
+camera.stop_preview()
+image = PIL.Image.open("camera_image.jpg")
+image_array = np.array(image)
+sock_sender.send_numpy_array(image_array)
 
-def send_file(file_name):
-    image = PIL.Image.open("camera_image.jpg")
-    image_array = np.array(image)
-    sock_sender.send_numpy_array(image_array)
+
+# def send_file(file_name):
+#     image = PIL.Image.open("camera_image.jpg")
+#     image_array = np.array(image)
+#     sock_sender.send_numpy_array(image_array)
+    
 
 
 # while True:
