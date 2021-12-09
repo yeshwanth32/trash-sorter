839c39fae5c753b316f1caf1351d3831837ff8a5
yeshwanth32 <yeshwanthreddy32@gmail.com>
2021-12-09 13:49:22 -0500

    scsdc

--- client.py
+++ client.py
@@ -15,18 +15,18 @@ in2 = 23
 en = 25
 temp1=1
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(in1,GPIO.OUT)
+# GPIO.setup(in2,GPIO.OUT)
+# GPIO.setup(en,GPIO.OUT)
+# GPIO.output(in1,GPIO.LOW)
+# GPIO.output(in2,GPIO.LOW)
+# p=GPIO.PWM(en,1000)
 
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
+# print("\n")
+# print("The default speed & direction of motor is LOW & Forward.....")
+# print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+# print("\n")    
 
 def stop():
     print("stop")
@@ -105,7 +105,6 @@ class Client:
             # #     while data:
             # #         self.s.send(data)
             # #         data = file.read(1024)
-            sleep(5)
             print("received response")
             recycling = True
             # if (recycling):
